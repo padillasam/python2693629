a27a507431b5ca2c92b46fdefd5d2b4fdb863cc0
padillasam <panarick@gmail.com>
2023-04-26 11:44:07 -0500

    ciclo for

---
+++ bucles/for1.py
@@ -0,0 +1,18 @@
+for i in range(11):
+    print(i)
+
+for j in range (1,11):
+    print(j)
+
+for k in range(0,11,2):
+    print(k)
+
+for i in range(10,-1,-2):
+    print(i)
+
+for i in range(11):
+    print('Amo programar en python')
+
+for i in range(1,11):
+    print(i**2)
+    
--- bucles/while3.py
+++ bucles/while3.py
@@ -1,8 +1,21 @@
 num=1
 cont=0
+sum=0
+mayor=0
+menor=1000000
 while num!=0:
     num=int(input('ingrese numero'))
     cont=cont+1 #cont+=1
+    sum=sum+num
+    if num>mayor:
+        mayor=num
+    if num<menor and num!=0:
+        menor=num
 
 print(f'El usuario ingreso {cont-1} numeros')
+print(f'La suma es {sum}')
+print(f'El promedio es {sum/(cont-1)} numeros')
+print(f'El mayor es {mayor}')
+print(f'El menor es {menor}')
 #print('El usuario ingreso', cont, 'numeros')
+
