effacc19606b3313bba0b847fd956e886c661c0c
padillasam <panarick@gmail.com>
2023-05-10 11:13:57 -0500

    arreglo burbuja

--- listas/burbuja2.py
+++ listas/burbuja2.py
@@ -3,9 +3,12 @@ tam=random.randint(5,10)
 lista=[random.randrange(100) for i in range(tam)]
 print(lista)
 
-for i in range(len(lista)-1):
-    if lista[i]<lista[i+1]:
-        lista[i],lista[i+1]=lista[i+1],lista[i]
+for i in range(tam-1):
+    for j in range(i+1,tam):
+        if lista[i]>lista[j]:
+            lista[i],lista[j]=lista[j],lista[i]
+
+print(lista)
 
 print(lista)
     
--- listas/tempCodeRunnerFile.py
+++ listas/tempCodeRunnerFile.py
@@ -1,1 +1,11 @@
-posicion-1
+import random
+tam=random.randint(5,10)
+lista=[random.randrange(100) for i in range(tam)]
+print(lista)
+
+for i in range(tam-1):
+    for j in range(i+1,tam):
+        if lista[i]>lista[j]:
+            lista[i],lista[j]=lista[j],lista[i]
+
+print(lista)
