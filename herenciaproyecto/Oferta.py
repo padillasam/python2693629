08a0e4e34eb4f6fdfb0649e0bac7ea5de9219997
padillasam <panarick@gmail.com>
2023-07-18 14:03:27 -0500

    conflict fixing 2

--- herenciaproyecto/Oferta.py
+++ herenciaproyecto/Oferta.py
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
 
 class Oferta:
     pass
     # def __init__(self,tipo,cargo):
     #     pass
-
-
-=======
-
-class Oferta:
-    pass
-    # def __init__(self,tipo,cargo):
-    #     pass
-
-
->>>>>>> 11cd638348011159777d0ff59ea7df5f04acf63f
