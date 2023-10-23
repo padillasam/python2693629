6da80ce4d6a230a343a1419200cdb2231d924fc2
padillasam <panarick@gmail.com>
2023-10-23 10:46:02 -0500

    headers

--- httptest/http4.py
+++ httptest/http4.py
@@ -2,14 +2,21 @@ import requests
 import json
 
 url="https://httpbin.org/post"#?nombre=juan&documento=12345"
-response=requests.post(url)
+argumentos={
+    'nombre':'Juan',
+    'documento':'12345'
+}
+
+#response=requests.post(url,json=argumentos)
+#response=requests.post(url,data=argumentos)
+response=requests.post(url,data=json.dumps(argumentos))
 #print(response.content.decode())
 decodetest=response.content.decode()
-print(type(decodetest))
-print(response.json())
-print('*'*20)
-decodetestjson=json.loads(response.content)
-print(decodetestjson)
+#print(type(decodetest))
+#print(response.json())
+#print('*'*20)
+#decodetestjson=json.loads(response.content)
+#print(decodetestjson)
 print('*'*20)
 print(decodetest)
 
---
+++ httptest/http5.py
@@ -0,0 +1,15 @@
+import requests
+import json
+url="https://httpbin.org/post"#?nombre=juan&documento=12345"
+argumentos={
+    'nombre':'Juan',
+    'documento':'12345'
+}
+theheaders={
+    'Content-Type':'Text',
+    'access-token':'12345'
+}
+response=requests.post(url,data=json.dumps(argumentos),headers=theheaders)
+decodetest=response.content.decode()
+print('*'*20)
+print(decodetest)
